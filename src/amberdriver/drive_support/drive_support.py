b410acf50477138a42659657988e9b8c89c299d9
Paweł Suder <pawel@suder.info>
2015-05-17 13:16:38 +0200

    Multiple changes in drive support

--- src/amberdriver/drive_support/drive_support.py
+++ src/amberdriver/drive_support/drive_support.py
@@ -4,9 +4,11 @@ import time
 
 import os
 from amberclient.common.listener import Listener
+
 from ambercommon.common import runtime
 
 from amberdriver.drive_support import drive_support_logic
+from amberdriver.drive_support.drive_support_logic import LowPassFilter, Accumulator, average
 from amberdriver.tools import config
 
 
@@ -17,6 +19,8 @@ logging.config.fileConfig('%s/drive_support.ini' % pwd)
 config.add_config_ini('%s/drive_support.ini' % pwd)
 
 LOGGER_NAME = 'DriveSupport'
+
+ROBO_WIDTH = float(config.ROBO_WIDTH)
 
 
 class ScanHandler(Listener):
@@ -35,14 +39,76 @@ class MotionHandler(Listener):
         self.__drive_support.set_motion(response)
 
 
+class Scan(object):
+    def __init__(self, points):
+        self.points = points
+        self.timestamp = time.time() * 1000.0
+
+    def __iter__(self):
+        return iter(self.points)
+
+
+class Motion(object):
+    def __init__(self, acc_forward, acc_side, speed_rotational):
+        self.accel_forward = acc_forward
+        self.accel_side = acc_side
+        self.speed_rotational = speed_rotational
+        self.timestamp = time.time() * 1000.0
+
+
+class Power(object):
+    def __init__(self, current, voltage):
+        self.current = current
+        self.voltage = voltage
+        self.timestamp = time.time() * 1000.0
+
+
+class Speed(object):
+    def __init__(self, fl, fr, rl, rr):
+        self.front_left, self.front_right = fl, fr
+        self.rear_left, self.rear_right = rl, rr
+        self.timestamp = time.time() * 1000.0
+
+    def __iter__(self):
+        return iter((self.front_left, self.front_right, self.rear_left, self.rear_right))
+
+
+def get_motion_data(motion):
+    accel = motion.get_accel()
+    gyro = motion.get_gyro()
+
+    # getting data from motion sensor specific for installation
+    acc_forward, acc_side, = accel.x_axis, accel.y_axis
+    speed_rotational = gyro.z_axis
+
+    return acc_forward, acc_side, speed_rotational
+
+
 class DriveSupport(object):
     def __init__(self, roboclaw_driver, hokuyo_proxy, ninedof_proxy):
-        self.__scan, self.__scan_ts = None, 0.0
-        self.__motion, self.__motion_ts = None, 0.0
-        self.__measured_speed, self.__measure_ts = None, 0.0
+        self.__scan = None
+        self.__motion = None
+        self.__speeds = None
+        self.__power = None
+        self.__user_speeds = None
 
-        self.__first_lpf = LowPassFilter([0.0] * 4)
-        self.__last_lpf = LowPassFilter([0.0] * 4)
+        self.__motion_filter = LowPassFilter(0.3, 0.0, 0.0, 0.0)
+        self.__speeds_filter = LowPassFilter(0.45, 0.0, 0.0, 0.0, 0.0)
+        self.__power_filter = LowPassFilter(0.2, 0.0, 0.0)
+        self.__user_speeds_filter = LowPassFilter(0.3, 0.0, 0.0, 0.0, 0.0)
+
+        self.__current_accumulator = Accumulator()
+        self.__voltage_accumulator = Accumulator()
+
+        self.__speed_computed_from_motion = 0.0
+        self.__radius_computed_from_motion = 0.0
+
+        self.__speed_computed_from_measurement = 0.0
+        self.__radius_computed_from_measurement = 0.0
+
+        self.__speed_limits = None
+        self.__environmental_forces = None
+        self.__current_environment_map = None
 
         self.__roboclaw_driver = roboclaw_driver
 
@@ -64,68 +130,100 @@ class DriveSupport(object):
     def terminate(self):
         self.__is_active = False
         self.__hokuyo_proxy.unsubscribe(self.__hokuyo_listener)
+        self.__ninedof_proxy.unsubscribe(self.__ninedof_listener)
 
     def stop(self):
         self.__roboclaw_driver.stop()
 
     def set_scan(self, scan):
-        self.__scan = scan.get_points()
-        self.__scan_ts = scan.get_timestamp()
+        # Filter (?) and create object
+        self.__scan = Scan(scan.get_points())
+
+        self.__speed_limits = drive_support_logic.compute_speed_limits(scan)
+        self.__environmental_forces = drive_support_logic.compute_environmental_forces(scan)
+        self.__current_environment_map = drive_support_logic.convert_map_polar_to_grid(scan)
 
     def set_motion(self, motion):
-        self.__motion = motion
-        self.__motion_ts = int(time.time() * 1000.0)
-
-    def set_speeds(self, front_left, front_right, rear_left, rear_right):
-        fl, fr, rl, rr = front_left, front_right, rear_left, rear_right
-        fl, fr, rl, rr = self.__drive_support(fl, fr, rl, rr)
-        self.__roboclaw_driver.set_speeds(fl, fr, rl, rr)
-
-    def get_measured_speeds(self):
-        return self.__measured_speed
+        accel_forward, accel_side, speed_rotational = get_motion_data(motion)
+        # Filter and create object
+        accel_forward, accel_side, speed_rotational = self.__motion_filter(accel_forward, accel_side, speed_rotational)
+        self.__motion = Motion(accel_forward, accel_side, speed_rotational)
+        # Compute forward speed and turn radius
+        speed = self.__motion.accel_side / self.__motion.speed_rotational
+        radius = speed / self.__motion.speed_rotational
+        self.__speed_computed_from_motion = speed
+        self.__radius_computed_from_motion = radius
 
     def measure_loop(self):
         while self.__is_active:
-            self.__measured_speed = self.__roboclaw_driver.get_measured_speeds()
-            self.__measure_ts = int(time.time() * 1000.0)
+            speeds = self.__roboclaw_driver.get_measured_speeds()
+            # Filter and create object
+            speeds = self.__speeds_filter(*speeds)
+            self.__speeds = Speed(*speeds)
+
+            current, voltage = self.__roboclaw_driver.get_current_and_voltage()
+            # Filter and create object
+            current, voltage = self.__power_filter(current, voltage)
+            self.__power = Power(current, voltage)
+
+            self.__current_accumulator.append(current)
+            self.__voltage_accumulator.append(voltage)
+
+            # detect changes on current and voltage
+
+            # Compute forward speed and turn radius
+            speed, radius = compute_speed_radius(speeds)
+            self.__speed_computed_from_measurement = speed
+            self.__radius_computed_from_measurement = radius
+
             time.sleep(0.1)
 
-    def __drive_support(self, fl, fr, rl, rr):
-        if self.__scan is None:
-            return 0, 0, 0, 0
+    def get_measured_speeds(self):
+        return self.__speeds
 
-        """
-        TODO(paoolo):
-        * filter motion values
-        * filter distances values
-        """
+    def set_speeds(self, front_left, front_right, rear_left, rear_right):
+        current_timestamp = time.time()
+        # Filter and create object
+        speeds = front_left, front_right, rear_left, rear_right
+        speeds = self.__user_speeds_filter(*speeds)
+        speeds = Speed(*speeds)
 
-        fl, fr, rl, rr = self.__first_lpf.compute([fl, fr, rl, rr])
+        # detect if oscillation exists
+        # reduce speed due to environment
+        # filter data
+        speeds = Speed(*self.__user_speeds(*speeds))
+        speed, radius = compute_speed_radius(speeds)
 
-        fl, fr, rl, rr = drive_support_logic.adjust_speed(fl, fr, rl, rr, self.__motion, self.__measured_speed)
-        fl, fr, rl, rr = drive_support_logic.limit_speed(fl, fr, rl, rr, self.__scan)
+        sd1 = self.__speed_computed_from_motion - speed
+        sd2 = self.__speed_computed_from_measurement - speed
+        rd1 = self.__radius_computed_from_motion - radius
+        rd2 = self.__radius_computed_from_measurement - radius
 
-        fl, fr, rl, rr = self.__last_lpf.compute([fl, fr, rl, rr])
+        trust_level = drive_support_logic.data_trust(self.__scan.timestamp / 1000.0, current_timestamp) * \
+                      drive_support_logic.data_trust(self.__motion.timestamp / 1000.0, current_timestamp) * \
+                      drive_support_logic.data_trust(self.__speeds.timestamp / 1000.0, current_timestamp) * \
+                      drive_support_logic.data_trust(self.__power.timestamp / 1000.0, current_timestamp)
+        speeds = map(lambda val: int(val * trust_level), speeds)
 
-        ts = time.time()
-        trust_level = drive_support_logic.data_trust(self.__scan_ts, ts) * \
-                      drive_support_logic.data_trust(self.__motion_ts, ts) * \
-                      drive_support_logic.data_trust(self.__measure_ts, ts)
-
-        fl *= trust_level
-        rl *= trust_level
-        fr *= trust_level
-        rr *= trust_level
-
-        return int(fl), int(fr), int(rl), int(rr)
+        self.__roboclaw_driver.set_speeds(*speeds)
 
 
-class LowPassFilter(object):
-    def __init__(self, values, alpha=0.3):
-        self.__values = values
-        self.__alpha = alpha
+def compute_speed_radius(speeds):
+    speed_left = average(speeds.front_left, speeds.rear_left)
+    speed_right = average(speeds.front_right, speeds.rear_right)
+    speed_front = average(speeds.front_left, speeds.front_right)
+    speed_rear = average(speeds.rear_left, speeds.rear_right)
 
-    def compute(self, values):
-        values = map(lambda (prev, curr): (prev + self.__alpha * (curr - prev)), zip(self.__values, values))
-        self.__values = values
-        return values
+    speed_left_right = average(speed_left, speed_right)
+    speed_front_rear = average(speed_front, speed_rear)
+
+    speed = average(speed_left_right, speed_front_rear)
+    radius = speed_left * ROBO_WIDTH / (speed_left - speed_right)
+
+    return speed, radius
+
+
+def compute_accel_side_speed_rotational(speed, radius):
+    speed_rotational = speed / radius
+    acc_side = speed_rotational * speed
+    return acc_side, speed_rotational
--- src/amberdriver/drive_support/drive_support_logic.py
+++ src/amberdriver/drive_support/drive_support_logic.py
@@ -12,6 +12,7 @@ pwd = os.path.dirname(os.path.abspath(__file__))
 config.add_config_ini('%s/drive_support.ini' % pwd)
 
 ROBO_WIDTH = float(config.ROBO_WIDTH)
+ROBO_MASS = float(config.ROBO_MASS)
 
 MAX_SPEED = float(config.MAX_SPEED)
 MAX_ROTATING_SPEED = float(config.MAX_ROTATING_SPEED)
@@ -23,6 +24,15 @@ ANGLE_RANGE = float(config.ANGLE_RANGE)
 
 DISTANCE_ALPHA = float(config.DISTANCE_ALPHA)
 RODEO_SWAP_ALPHA = float(config.RODEO_SWAP_ALPHA)
+
+""" Other """
+
+
+def average(*values):
+    return (float(sum(values)) / float(len(values))) if len(values) > 0 else 0.0
+
+
+""" Angles function, conversion, etc.  """
 
 
 def get_angle(left, right, robo_width):
@@ -37,197 +47,21 @@ def normalize_angle(angle):
     return angle
 
 
-def get_speed(left, right):
-    return (left + right) / 2.0
+""" Trust data function """
 
 
-def get_max_speed(distance, soft_limit, hard_limit, max_speed):
-    return max_speed / (soft_limit - hard_limit) * float(distance) - \
-           (max_speed * hard_limit) / (soft_limit - hard_limit)
-
-
-def get_soft_limit(current_speed, max_speed, soft_limit, hard_limit, alpha):
-    return alpha * soft_limit * (current_speed / max_speed) + hard_limit + 50.0
-
-
-def convert_angles_to_radians(points):
-    return map(lambda (angle, distance): (math.radians(angle), distance), points)
-
-
-def convert_angles_to_degrees(points):
-    return map(lambda (angle, distance): (math.degrees(angle), distance), points)
-
-
-def get_min_distance(scan, current_angle, scanner_dist_offset, angle_range):
-    points = convert_angles_to_radians(scan)
-    min_distance = None
-    min_distance_angle = None
-
-    for angle, distance in points:
-        if distance > scanner_dist_offset \
-                and current_angle - angle_range < angle < current_angle + angle_range:
-            if min_distance is None or distance < min_distance:
-                min_distance = distance
-                min_distance_angle = angle
-
-    return min_distance, min_distance_angle
-
-
-def get_max_distance(scan, current_angle, scanner_dist_offset, angle_range):
-    points = convert_angles_to_radians(scan)
-    max_distance = None
-    max_distance_angle = None
-    max_diff_angle = 0.0
-
-    for angle, distance in points:
-        if (distance > scanner_dist_offset or distance == 0) \
-                and current_angle - angle_range < angle < current_angle + angle_range:
-            diff_angle = abs(current_angle - angle)
-            if (max_distance is None or distance > max_distance or distance == 0) and diff_angle > max_diff_angle:
-                max_distance = distance
-                max_distance_angle = angle
-                max_diff_angle = diff_angle
-
-    return max_distance, max_distance_angle
-
-
-def simple_importance_angle_level(angle):
-    if math.fabs(angle) < math.pi / 2.0:
-        return 1.0 - 2.0 * math.fabs(angle) / math.pi
-    return 0.0
-
-
-def simple_distance_level(distance, max_distance=5000.0):
-    """ max_distance in [mm] """
-    return distance / max_distance
-
-
-def average(values):
-    return reduce(lambda x, y: x + y, values) / len(values)
-
-
-def distance_danger_level(scan,
-                          importance_angle_level_func=simple_importance_angle_level,
-                          distance_level_func=simple_distance_level,
-                          compute_level_func=average):
-    points = convert_angles_to_radians(scan)
-    levels = []
-    for angle, distance in points:
-        importance_angle_level = importance_angle_level_func(angle)
-        distance_level = distance_level_func(distance)
-        levels.append(importance_angle_level * distance_level)
-    return compute_level_func(levels)
-
-
-def find_continuities(scan, max_continuity_interval=10):
-    """ max_continuity_interval in [mm] for distance 100 [mm] """
-    points = convert_angles_to_radians(scan)
-    continuities = []
-    old_x, old_y = None, None
-    first_x, first_y = None, None
-    for angle, distance in sorted(points.items()):
-        x, y = distance * math.cos(angle), distance * math.sin(angle)
-        if old_x is None and old_y is None:
-            old_x, old_y = x, y
-            first_x, first_y = x, y
-        else:
-            limit = math.pow(distance * max_continuity_interval * 0.01, 2)
-            if math.pow(old_x - x, 2) > limit or math.pow(old_y - y, 2) > limit:
-                first = (first_x, first_y)
-                last = (old_x, old_y)
-                continuities.append((first, last))
-                first_x, first_y = x, y
-            else:
-                old_x, old_y = x, y
-    return continuities
-
-
-def find_locals_min_max(scan, interval=50, min_distance=100.0, max_distance=5000.0):
-    """ interval in 50 [mm] """
-    points = convert_angles_to_radians(scan)
-    local_mini, local_maxi = None, None
-    local_minis, local_maxis = [], []
-    prev_distance = None
-    for angle, distance in sorted(points.items()):
-        if min_distance < distance < max_distance:
-            if prev_distance is None:
-                prev_distance = distance
-            else:
-                if distance - prev_distance > interval:
-                    local_maxi = (angle, distance)
-                    if local_mini is not None:
-                        local_minis.append(local_mini)
-                        local_mini = None
-                elif distance - prev_distance < -interval:
-                    local_mini = (angle, distance)
-                    if local_maxi is not None:
-                        local_maxis.append(local_maxi)
-                        local_maxi = None
-                prev_distance = distance
-    return local_minis, local_maxis
-
-
-def limit_due_to_distance(left, right, scan):
-    if left > 0 or right > 0:
-        current_angle = get_angle(left, right,
-                                  ROBO_WIDTH)
-        current_speed = get_speed(left, right)
-
-        if scan is not None:
-            min_distance, _ = get_min_distance(scan, current_angle,
-                                               SCANNER_DIST_OFFSET, ANGLE_RANGE)
-
-            if min_distance is not None:
-                soft_limit = get_soft_limit(current_speed,
-                                            MAX_SPEED, SOFT_LIMIT * 1.3, HARD_LIMIT * 1.3, DISTANCE_ALPHA)
-
-                if HARD_LIMIT * 1.3 < min_distance < soft_limit:
-                    max_speed = get_max_speed(min_distance, soft_limit,
-                                              HARD_LIMIT * 1.3, MAX_SPEED)
-                    if current_speed > max_speed:
-                        left, right = __calculate_new_left_right(left, right, max_speed, current_speed)
-
-                elif min_distance <= HARD_LIMIT * 1.3:
-                    left, right = 0, 0
-
-        else:
-            print 'distance: no scan!'
-            left, right = 0.0, 0.0
-
-    return left, right
-
-
-def __calculate_new_left_right(left, right, max_speed, current_speed):
-    if current_speed > 0:
-        divide = max_speed / current_speed
-        return left * divide, right * divide
-    else:
-        return left, right
-
-
-def limit_to_max_speed(left, right):
-    left = __limit_to_max_speed(left)
-    right = __limit_to_max_speed(right)
-
-    return left, right
-
-
-def __limit_to_max_speed(value):
-    max_speed = MAX_SPEED
-    return max_speed if value > max_speed \
-        else -max_speed if value < -max_speed \
-        else value
-
-
-def data_trust(data_ts, curr_ts):
-    val = data_ts / 1000.0 - curr_ts
-    return math.pow(4.0 / 3.0, val)
+def data_trust(data_ts, curr_ts, exp_base=4.0 / 3.0):
+    val = data_ts - curr_ts
+    return math.pow(exp_base, val)
 
 
 def location_trust(location):
     _, _, location_probability, _, location_timestamp = location
     current_timestamp = time.time()
-    return location_probability * data_trust(location_timestamp, current_timestamp)
+    return location_probability * data_trust(location_timestamp / 1000.0, current_timestamp)
+
+
+""" Data polar/grid functions, conversion, etc. """
 
 
 def convert_grid_to_polar(x, y):
@@ -266,73 +100,99 @@ def convert_map_polar_to_grid(map_polar):
     return map_grid
 
 
-class Map(object):
-    def __init__(self):
-        self.__map = {}
-
-    def add(self, x, y, angle=None, distance=None):
-        if angle is not None and distance is not None:
-            _x, _y = convert_polar_to_grid(distance, angle)
-            x += _x
-            y += _y
-        if x not in self.__map:
-            self.__map[x] = {}
-        if y not in self.__map[x]:
-            self.__map[x][y] = time.time()
+""" Speed function, limitation, etc. """
 
 
-def motion_to_speed(motion,
-                    acc_function=lambda acc: acc,
-                    rev_function=lambda rev: rev):
-    accel = motion.get_accel()
-    gyro = motion.get_gyro()
-
-    acc_x, acc_y = accel.x_axis, accel.y_axis
-    acc_x, acc_y = acc_function(acc_x), acc_function(acc_y)
-    acc, angle = convert_grid_to_polar(acc_x, acc_y)
-
-    gyro_val = gyro.z_axis
-    rev = rev_function(gyro_val)
-
-    if not gyro_val == 0:
-        pass
-
-        # acc -> motion in any way
-        # gyro -> turn left or right
+def get_speed(left, right):
+    return (left + right) / 2.0
 
 
-def speed_to_motion(speed):
-    pass
+def get_radius(left, right):
+    return (right * ROBO_WIDTH) / (left - right) + ROBO_WIDTH / 2.0
 
 
-def compute_obstacle_forces(map_polar,
-                            distance_function=lambda (distance, angle): distance,
-                            force_function=lambda (force_x, force_y, angle): (force_x, force_y)):
-    sum_force_x, sum_force_y = 0.0, 0.0
+def get_centripetal_force(velocity, radius):
+    return (ROBO_MASS * velocity ** 2) / radius
+
+
+def get_hard_limit(distance, soft_limit, hard_limit, max_speed):
+    return max_speed / (soft_limit - hard_limit) * float(distance) - \
+           (max_speed * hard_limit) / (soft_limit - hard_limit)
+
+
+def get_soft_limit(current_speed, max_speed, soft_limit, hard_limit, alpha):
+    return alpha * soft_limit * (current_speed / max_speed) + hard_limit + 50.0
+
+
+def distance_to_speed(distance, angle, angle_offset=0.0,
+                      distance_function=lambda d: d,
+                      angle_function=lambda a: a):
+    return angle_function(angle) * distance_function(distance)
+
+
+def compute_speed_limits(map_polar,
+                         distance_function=distance_to_speed):
+    speed_limits = []
     for angle, distance in map_polar:
         distance = distance_function(distance, angle)
-        force_x, force_y = convert_polar_to_grid(distance, angle)
-        force_x, force_y = force_function(force_x, force_y, angle)
-        sum_force_x += force_x
-        sum_force_y += force_y
+        speed_limits.append((angle, distance))
+    return speed_limits
 
 
-def compute_target_force():
+def distance_to_force(distance, angle, angle_offset=0.0,
+                      distance_function=lambda d: d,
+                      angle_function=lambda a: a):
+    return angle_function(angle_function) * distance_function(distance)
+
+
+def compute_environmental_forces(map_polar,
+                                 distance_to_force_function=distance_to_force):
+    forces_polar = []
+    for angle, distance in map_polar:
+        force = distance_to_force_function(distance, angle)
+        forces_polar.append((angle, force))
+    return forces_polar
+
+
+""" Additional filter, etc. """
+
+
+class LowPassFilter(object):
+    def __init__(self, alpha, *args):
+        self.__values = args
+        self.__alpha = alpha
+
+    def __call__(self, *args):
+        self.__values = map(lambda (prev, curr): (prev + self.__alpha * (curr - prev)), zip(self.__values, args))
+        return self.__values
+
+
+class DelayingFilter(object):
     pass
 
 
-def adjust_speed(fl, fr, rl, rr, motion_data, measured_speed):
-    """
-    * analyze robo motion: from gyro/accel and measured speed
-    * adjust speed
-    """
-    return fl, fr, rl, rr
+class KalmanFilter(object):
+    pass
 
 
-def limit_speed(fl, fr, rl, rr, environment_scan):
-    """
-    * lookahead around robot
-    * verify state
-    * limit if necessary
-    """
-    return fl, fr, rl, rr
+class Accumulator(list):
+    def __init__(self, values=list(), max_length=10):
+        super(Accumulator, self).__init__(values)
+        self.__max_length = max_length
+
+    def append(self, x):
+        super(Accumulator, self).append(x)
+        if len(self) > self.__max_length:
+            self.pop(0)
+
+    def average_diff(self):
+        return average(*self.diffs())
+
+    def diffs(self):
+        diffs = []
+        if len(self) > 1:
+            _prev = self[0]
+            for _next in self[1:]:
+                diffs.append(_next - _prev)
+                _prev = _next
+        return diffs
